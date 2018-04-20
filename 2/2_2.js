b0cb38960ddcc6e7293e5756020ff2c3b0d71f40
Deliaz <D3liaz@gmail.com>
2018-04-20 23:51:27 +0700

    Solutions for chapter 2: 2.2

---
+++ 2/2_2.js
@@ -0,0 +1,69 @@
+const LinkedList = require('./LinkedList');
+
+module.exports = {
+
+    /**
+     * Collect node to stack and get the Kth to the last
+     * O(n) space and time
+     * @param node
+     * @param k
+     * @returns {*}
+     */
+    find(node, k) {
+        if (!node) {
+            return null;
+        }
+        const stack = [];
+
+        while (node !== null) {
+            stack.push(node);
+            node = node.next;
+        }
+
+        let kth;
+        if (k && k !== 1) { // if not zero
+            while (k--) {
+                kth = stack.pop();
+            }
+        } else {
+            kth = stack[stack.length - 1]; // last element
+        }
+
+        return kth;
+    },
+
+
+    /**
+     * Iterative way with two pointers
+     * @param node
+     * @param k
+     * @returns {number|null}
+     */
+    find2(node, k) {
+        if (node === null) {
+            return 0;
+        }
+
+        let p1 = node;
+        let p2 = node;
+
+        for (let i = 0; i < k; i++) {
+            p1 = p1.next;
+            if (p1 === null) {
+                return null;
+            }
+        }
+
+        while (p1 !== null) {
+            p1 = p1.next;
+
+            if (p2.next !== null) { // in case if k equals to zero
+                p2 = p2.next;
+            }
+
+        }
+
+        return p2;
+    }
+};
+
---
+++ 2/2_2.test.js
@@ -0,0 +1,87 @@
+const expect = require('chai').expect;
+const {find, find2} = require('./2_2');
+const LinkedList = require('./LinkedList');
+
+
+describe('2.1 Remove Dups', () => {
+
+    // Find 1
+    it('should return 2nd to last node (value 6)', () => {
+        const list = new LinkedList(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+        list.append(6); // 2 <--
+        list.append(7); // 1
+
+        const kth = find(list, 2);
+        expect(kth.data).to.equal(6);
+    });
+
+    it('should return last element by index 1', () => {
+        const list = new LinkedList(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+        list.append(6);
+        list.append(7); // 1 <--
+
+        const kth = find(list, 1);
+        expect(kth.data).to.equal(7);
+    });
+
+    it('should return last element by index 0', () => {
+        const list = new LinkedList(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+        list.append(6);
+        list.append(7); // 0 <--
+
+        const kth = find(list, 0);
+        expect(kth.data).to.equal(7);
+    });
+
+    // Find 2
+    it('should return 2nd to last node (value 6)', () => {
+        const list = new LinkedList(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+        list.append(6); // 2 <--
+        list.append(7); // 1
+
+        const kth = find2(list, 2);
+        expect(kth.data).to.equal(6);
+    });
+
+    it('should return last element by index 1', () => {
+        const list = new LinkedList(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+        list.append(6);
+        list.append(7); // 1 <--
+
+        const kth = find2(list, 1);
+        expect(kth.data).to.equal(7);
+    });
+
+    it('should return last element by index 0', () => {
+        const list = new LinkedList(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+        list.append(6);
+        list.append(7); // 0 <--
+
+        const kth = find2(list, 0);
+        expect(kth.data).to.equal(7);
+    });
+});
